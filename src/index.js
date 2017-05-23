0765b44ab76694450b24076781ab28d190eeac33
izhigalov <rim10@inbox.ru>
2017-05-23 20:32:49 +0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,108 @@
+/* ДЗ 3 - работа с массивами и объеектами */
+
+/*
+ Задача 1:
+ Напишите аналог встроенного метода forEach для работы с массивами
+ */
+function forEach(array, fn) {
+	for (var i = 0; i < array.lenght; i++){
+		fn(array[i]);
+	}
+}
+
+/*
+ Задача 2:
+ Напишите аналог встроенного метода map для работы с массивами
+ */
+function map(array, fn) {
+	var mapArr = function(array, fn, thisArg) {
+  var i, length = arr.length, results = [];
+  for (var i = 0; i < length; i++) {
+    results.push(fn.call(thisArg, arr[i], i, arr));
+  }
+  return results;
+};
+}
+
+/*
+ Задача 3:
+ Напишите аналог встроенного метода reduce для работы с массивами
+ */
+function reduce(array, fn, initial) {
+}
+
+/*
+ Задача 4:
+ Функция принимает объект и имя свойства, которое необходиом удалить из объекта
+ Функция должна удалить указанное свойство из указанного объекта
+ */
+function deleteProperty(obj, prop) {
+	if (obj.hasOwnProperty('prop')){
+		delete obj.prop; 
+	}
+}
+
+/*
+ Задача 5:
+ Функция принимает объект и имя свойства и возвращает true или false
+ Функция должна проверить существует ли укзаанное свойство в указанном объекте
+ */
+function hasProperty(obj, prop) {
+	for (prop in obj) {
+		if (obj.hasOwnProperty('prop')){
+			return hasProperty();
+		}
+	}
+	
+}
+
+/*
+ Задача 6:
+ Функция должна получить все перечисляемые свойства объекта и вернуть их в виде массива
+ */
+function getEnumProps(obj) {
+	var arr = [];
+	for (var key in obj) {
+		arr.push(key)		
+	}
+	return arr;
+}
+
+/*
+ Задача 7:
+ Функция должна перебрать все свойства объекта, преобразовать их имена в верхний регистра и вернуть в виде массива
+ */
+function upperProps(obj) {
+	var obj1 = [];
+	for (var key in obj) {
+		obj1.push(key.toUpperCase())
+	}
+	return obj1;
+}
+
+/*
+ Задача 8 *:
+ Напишите аналог встроенного метода slice для работы с массивами
+ */
+function slice(array, from, to) {
+}
+
+/*
+ Задача 9 *:
+ Функция принимает объект и должна вернуть Proxy для этого объекта
+ Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
+ */
+function createProxy(obj) {
+}
+
+export {
+    forEach,
+    map,
+    reduce,
+    deleteProperty,
+    hasProperty,
+    getEnumProps,
+    upperProps,
+    slice,
+    createProxy
+};

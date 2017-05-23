c83cb841c5b71a2834080c41043dfaf93667e1d0
izhigalov <rim10@inbox.ru>
2017-05-23 20:24:46 +0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,81 @@
+/* ДЗ 1 - Функции */
+
+/*
+ Задание 1:
+
+ Функция должна принимать один аргумент и возвращать его
+ */
+function returnFirstArgument(arg) {
+    return arg
+}
+console.log(returnFirstArgument(4));
+
+
+/*
+ Задание 2:
+
+ Функция должна принимать два аргумента и возвращать сумму переданных значений
+ Значение по умолчанию второго аргумента должно быть 100
+ */
+function defaultParameterValue(a, b = 100) {
+    return a + b
+}
+console.log(defaultParameterValue(10));
+
+/*
+ Задание 3:
+
+ Функция должна возвращать все переданные в нее аргументы в виде массива
+ Количество переданных аргументов заранее неизвестно
+ */
+function returnArgumentsArray() {
+    var array = [];
+    for (var i=0; i < arguments.length; i++) {
+        array.push(arguments[i])
+    }
+    return array;
+}
+returnArgumentsArray(1, 2, 3);
+console.log(returnArgumentsArray(1, 2, 3));
+
+/*
+ Задание 4:
+
+ Функция должна принимать другую функцию и возвращать результат вызова переданной функции
+ */
+function returnFnResult(fn) {
+    return function(fn)
+}
+
+
+
+/*
+ Задание 5:
+
+ Функция должна принимать число (значение по умолчанию - 0) и возвращать функцию (F)
+ При вызове F, переданное число должно быть увеличено на единицу и возвращено из F
+ */
+function returnCounter(number = 0) {
+    return function f(){
+        return ++number;
+    }
+}
+// console.log(returnCounter);
+
+/*
+ Задание 6 *:
+
+ Функция должна принимать другую функцию (F) и некоторое количество дополнительных аргументов
+ Функция должна привязать переданные аргументы к функции F и вернуть получившуюся функцию
+ */
+function bindFunction(fn) {
+}
+
+export {
+    returnFirstArgument,
+    defaultParameterValue,
+    returnArgumentsArray,
+    returnFnResult,
+    returnCounter,
+    bindFunction
+}
